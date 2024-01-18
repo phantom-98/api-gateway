34bc7bd198dbbcd30c51073605e74702e636c6bc
samuelborraccini <samuelborraccini@hotmail.com>
2024-01-18 12:57:53 -0300

    add proxyHandler

--- src/index.js
+++ src/index.js
@@ -4,15 +4,9 @@ import "./db/prismaClient.js"
 import 'dotenv/config.js'
 import userRouter from './routes/auth.js'
 import Profilesrouter from './routes/profile.js'
-import axios from 'axios'
-import httpProxy from "http-proxy"
-import createProxyHandler from './utils/proxyHandler.js'
+import createProxyHandler from "./utils/proxyHandler.js"
 const app = express()
-const apiProxy = httpProxy.createProxyServer();
-apiProxy.on('error', (err, req, res) => {
-    console.error(err);
-    res.status(500).send('Proxy error');
-});
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded());
@@ -22,7 +16,7 @@ app.get('/test',(req,res) => {
     console.log(req.url);
     res.json({message:'test'})
 })
-app.all("/v1/*", createProxyHandler(process.env.API_URL))
+app.all("/v1/*", createProxyHandler('http://localhost:4000'))
 app.use('/auth',userRouter)
 app.use('/profile',Profilesrouter)
 app.listen(4001, () => {
