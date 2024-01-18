cded8dc301965ed8778761c067d49ad41cd58877
samuelborraccini <samuelborraccini@hotmail.com>
2024-01-18 13:54:47 -0300

    change origin in proxy

--- src/utils/proxyHandler.js
+++ src/utils/proxyHandler.js
@@ -3,11 +3,12 @@ const apiProxy = httpProxy.createProxyServer();
 
 function createProxyHandler(targetUrl) {
     return (req, res) => {
-        apiProxy.web(req, res, { target: targetUrl }, (error) => {
-            if (error) {
-                console.error('Proxy error:', error);
-                res.status(500).send('Proxy error');
+        apiProxy.web(req, res, { target: targetUrl,changeOrigin: true }, (error) => {
+            console.error('Proxy error:', error);
+            if (error.code) {
+                console.error('Proxy error code:', error.code);
             }
+            res.status(500).send('Proxy error');
         });
     };
 }
